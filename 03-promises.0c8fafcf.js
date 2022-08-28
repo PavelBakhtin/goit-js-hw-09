e786853965f9bfe2f27cdead6f46a4208ae7d666
PavelBakhtin <99673826+PavelBakhtin@users.noreply.github.com>
2022-08-28 10:28:22 +0000

    Deploying to gh-pages from @ PavelBakhtin/goit-js-hw-09@c60fd6b26ebb27960b16191a2d0dce60bcb982a3 🚀

--- 03-promises.0c8fafcf.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("delay"),document.querySelector("step"),document.querySelector("amount"),document.querySelector("submit");
-//# sourceMappingURL=03-promises.0c8fafcf.js.map
--- 03-promises.b06cb92b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("delay"),document.querySelector("step"),document.querySelector("amount"),document.querySelector("submit");
-//# sourceMappingURL=03-promises.b06cb92b.js.map
